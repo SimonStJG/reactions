ab5fc9bd8635723df58a78c219c708a6aa89a3ff
Simon StJG <Simon.StJG@gmail.com>
2022-03-27 15:33:28 +0100

    Add timeout, uses curses in a worse way?

--- reactions/__main__.py
+++ reactions/__main__.py
@@ -1,10 +1,9 @@
 """Test your reaction speed on a silly game with large buttons and ridiculous noises
 
 TODO
-* KEY_RESIZE
-* ESC and ESC delay
+* SIGWINCH / KEY_RESIZE not delivered when terminal is resized
+* The way I'm using curses can't possibly be right..  why do I have to redraw the footer??
 * Sound
-* Timeout at 100 secs
 """
 import asyncio
 import curses
@@ -18,6 +17,7 @@ import time
 _WIN_COLS = 80
 _WIN_LINES = 20
 _ROUNDS = 5
+_TIMEOUT_SECS = datetime.timedelta(seconds=60)
 
 _BUTTONS = {
     0: "q",
@@ -30,7 +30,6 @@ _BUTTONS = {
 }
 
 
-# todo move onto the state?
 @dataclasses.dataclass
 class Scores:
     current: datetime.timedelta = datetime.timedelta()
@@ -58,24 +57,42 @@ class State:
 
 async def main_loop(stdscr):
     stdscr.clear()
+
+    # Don't block when calling getch or getstr methods.
     stdscr.nodelay(True)
+
+    # Set the time to wait for an escape sequence after the ESC key is pressed to a tiny value.  We don't need any
+    # escape sequences want to use ESC to signal that the game is over.
+    curses.set_escdelay(10)
+
+    # Remove flashing cursor
+    curses.curs_set(0)
 
     validate_screen_size()
 
     scores = Scores()
+
+    # Create some windows to hold the different bits of text on the screen.  win_scores is the top line and has current
+    # and high score, win_footer is the bottom line and has some constant text, and win_main has everything else.
     win_scores = curses.newwin(1, _WIN_COLS, 0, 0)
-    win_main = curses.newwin(_WIN_LINES - 1, _WIN_COLS, 1, 0)
+    win_footer = curses.newwin(1, _WIN_COLS, _WIN_LINES - 1, 0)
+    win_main = curses.newwin(_WIN_LINES - 2, _WIN_COLS, 1, 0)
+
     last_tick = time.time_ns()
     state = State.NOT_STARTED
 
     while True:
         last_tick, time_elapsed = await _calculate_time_elapsed(last_tick)
 
-        keys = await read_keys(stdscr)
+        keys, is_exit = await read_keys(stdscr)
+        if is_exit:
+            break
+
         state = await tick(state, scores, keys, time_elapsed)
 
         refresh_win_scores(win_scores, scores)
         refresh_win_main(win_main, state)
+        refresh_win_footer(win_footer)
 
         await asyncio.sleep(0.01)
 
@@ -89,42 +106,52 @@ async def _calculate_time_elapsed(last_tick):
 
 async def read_keys(stdscr):
     keys = []
+    is_exit = False
     while True:
         key = stdscr.getch()
         if key == curses.ERR:
             break
 
-        if key == curses.KEY_RESIZE:
-            raise ValueError("TODO Handle resize")
+        if key == curses.ascii.ESC:
+            is_exit = True
 
         if key < 255:
             keys.append(chr(key))
 
-    return keys
+    return keys, is_exit
+
+
+def refresh_win_footer(win_footer):
+    win_footer.move(0, 0)
+    win_footer.clrtoeol()
+    win_footer.addstr(0, 0, "Press Escape key to exit")
+    win_footer.refresh()
 
 
 def refresh_win_scores(win_scores, scores):
-    win_scores.erase()
+    win_scores.move(0, 0)
+    win_scores.clrtoeol()
+
     win_scores.addstr(0, 0, f"{format_score(scores.current)} <- Current score")
     high_score_msg = f"High Score -> {format_score(scores.high)}"
     win_scores.addstr(0, _WIN_COLS - len(high_score_msg) - 1, high_score_msg)
     win_scores.refresh()
 
 
-def format_score(score):
-    return f"{score.seconds:02d}:{math.floor(score.microseconds / 10_000):02}"
-
-
 def refresh_win_main(win_main, state):
+    y_position = math.ceil((_WIN_LINES - 2) / 2)
+
     def centre_message(message):
         win_main.addstr(
-            math.ceil(_WIN_LINES / 2), math.ceil((_WIN_COLS - len(message)) / 2), message
+            y_position, math.ceil((_WIN_COLS - len(message)) / 2), message
         )
 
-    win_main.erase()
+    win_main.move(y_position, 0)
+    win_main.clrtoeol()
+
     match state:
         case State.NOT_STARTED:
-            centre_message("Press any key to start")
+            centre_message("Press space bar to start")
         case State.CoolDown():
             centre_message("Get Ready")
         case State.WaitingOnButton(button=button):
@@ -133,21 +160,19 @@ def refresh_win_main(win_main, state):
             centre_message(f"Your score: {format_score(last_score)}.  Press any key to play again.")
         case _:
             raise NotImplementedError(state)
+
     win_main.refresh()
+
+
+def format_score(score):
+    return f"{score.seconds:02d}:{math.floor(score.microseconds / 10_000):02}"
 
 
 async def tick(state, scores, keys, time_elapsed):
     match state:
         case State.NOT_STARTED:
-            if keys:
-                # TODO Dedupe
-                return State.CoolDown(
-                    round_=0,
-                    delay=datetime.timedelta(
-                        seconds=random.randint(2000, 4000) / 1_000
-                    ),
-                    elapsed=datetime.timedelta(),
-                )
+            if " " in keys:
+                return cool_down(round_=0)
 
         case State.CoolDown(round_=round_, delay=delay, elapsed=elapsed):
             new_elapsed = elapsed + time_elapsed
@@ -160,34 +185,36 @@ async def tick(state, scores, keys, time_elapsed):
 
         case State.WaitingOnButton(round_=round_, button=button):
             scores.current += time_elapsed
+
+            if scores.current >= _TIMEOUT_SECS:
+                return State.GameFinished(last_score=scores.current)
+
             if keys:
                 first_key = keys[0]
                 if first_key != button:
                     scores.current += datetime.timedelta(seconds=5)
                 if round_ == _ROUNDS:
                     return State.GameFinished(last_score=scores.current)
-                return State.CoolDown(
-                    round_=round_ + 1,
-                    delay=datetime.timedelta(
-                        seconds=random.randint(2000, 4000) / 1_000
-                    ),
-                    elapsed=datetime.timedelta(),
-                )
+                return cool_down(round_=round_ + 1)
 
         case State.GameFinished():
             if keys:
-                State.CoolDown(
-                    round_=0,
-                    delay=datetime.timedelta(
-                        seconds=random.randint(2000, 4000) / 1_000
-                    ),
-                    elapsed=datetime.timedelta(),
-                )
+                return cool_down(round_=0)
 
         case _:
             raise NotImplementedError(state)
 
     return state
+
+
+def cool_down(round_):
+    return State.CoolDown(
+        round_=round_,
+        delay=datetime.timedelta(
+            seconds=random.randint(2000, 4000) / 1_000
+        ),
+        elapsed=datetime.timedelta(),
+    )
 
 
 def validate_screen_size():
